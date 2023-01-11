1bda19c13eb42fc2420cf982d12f26df545d452e
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-01-10 23:31:29 -0700

    Add project files.

---
+++ FileHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeGenrePlaylist
+{
+    public static class FileHelper
+    {
+        public static ArrayList GetFilesBySearchPattern(string filePath, string searchPattern, SearchOption searchOption)
+        {
+            if (string.IsNullOrEmpty(searchPattern))
+                return null;
+
+            string[] arrExt = searchPattern.Split('|');
+            ArrayList arrLstFiles = new ArrayList();
+            for (int i = 0; i < arrExt.Length; i++)
+            {
+                arrLstFiles.AddRange(Directory.GetFiles(filePath, arrExt[i], searchOption));
+            }
+            return arrLstFiles;
+        }
+
+        public static ArrayList GetFilesByProperty(ArrayList files, string fileProperty, string filePropertyValue)
+        {
+            ArrayList arrLstFiles = new ArrayList();
+            foreach (string file in files)
+            {
+                if (filePropertyValue.Equals(GetExtendedFileProperty(file, fileProperty), StringComparison.OrdinalIgnoreCase))
+                {
+                    arrLstFiles.Add(file);
+                }
+            }
+            return arrLstFiles;
+        }
+
+        public static string GetExtendedFileProperty(string filePath, string propertyName)
+        {
+            string value = string.Empty;
+            string baseFolder = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+
+            //Method to load and execute the Shell object for Windows server 8 environment otherwise you get "Unable to cast COM object of type 'System.__ComObject' to interface type 'Shell32.Shell'"
+            Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
+            Object shell = Activator.CreateInstance(shellAppType);
+            Shell32.Folder shellFolder = (Shell32.Folder)shellAppType.InvokeMember("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { baseFolder });
+
+            //ParseName will find the specific file I'm looking for in the Shell32.Folder object
+            Shell32.FolderItem folderitem = shellFolder.ParseName(fileName);
+            if (folderitem != null)
+            {
+                for (int i = 0; i < short.MaxValue; i++)
+                {
+                    //Get the property name for property index i
+                    string property = shellFolder.GetDetailsOf(null, i);
+
+                    //Will be empty when all possible properties has been looped through, break out of loop
+                    if (String.IsNullOrEmpty(property)) break;
+
+                    //Skip to next property if this is not the specified property
+                    if (property == propertyName)
+                    {
+                        //Read value of property
+                        value = shellFolder.GetDetailsOf(folderitem, i);
+                        break;
+                    }
+                }
+            }
+            //returns string.Empty if no value was found for the specified property
+            return value;
+        }
+    }
+}
---
+++ XmlHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace MakeGenrePlaylist
+{
+    public static class XmlHelper
+    {
+        public static void CreatePlaylist(string name, string filePath, ArrayList musicFiles)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings()
+            {
+                Indent = true,
+                OmitXmlDeclaration = true,
+            };
+
+            using (XmlWriter writer = XmlWriter.Create(filePath, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteRaw("<?wpl version=\"1.0\"?>\n");
+                writer.WriteStartElement("smil");
+                writer.WriteStartElement("head");
+                writer.WriteStartElement("meta");
+                writer.WriteAttributeString("name", "Generator");
+                writer.WriteAttributeString("content", "Microsoft Windows Media Player -- 12.0.19041.1566");
+                writer.WriteEndElement();
+                writer.WriteStartElement("meta");
+                writer.WriteAttributeString("name", "ItemCount");
+                writer.WriteAttributeString("content", musicFiles.Count.ToString());
+                writer.WriteEndElement();
+                writer.WriteElementString("title", name);
+                writer.WriteEndElement(); // head
+                writer.WriteStartElement("body");
+                writer.WriteStartElement("seq");
+                foreach (string file in musicFiles)
+                {
+                    writer.WriteStartElement("media");
+                    writer.WriteAttributeString("src", Escape(file));
+                    writer.WriteAttributeString("albumTitle", Escape(FileHelper.GetExtendedFileProperty(file, "Album")));
+                    writer.WriteAttributeString("albumArtist", Escape(FileHelper.GetExtendedFileProperty(file, "Contributing artists")));
+                    writer.WriteAttributeString("trackTitle", Escape(FileHelper.GetExtendedFileProperty(file, "Title")));
+                    writer.WriteAttributeString("trackArtist", Escape(FileHelper.GetExtendedFileProperty(file, "Authors")));
+                    string lengthInTime = FileHelper.GetExtendedFileProperty(file, "Length");
+                    TimeSpan timeSpan = TimeSpan.ParseExact(lengthInTime, @"hh\:mm\:ss", CultureInfo.InvariantCulture);
+                    writer.WriteAttributeString("duration", timeSpan.TotalMilliseconds.ToString());
+                    writer.WriteEndElement(); // media
+                }
+                writer.WriteEndElement(); // body
+                writer.WriteEndElement(); // seq
+                writer.WriteEndElement(); // smil
+                writer.WriteEndDocument();
+            }
+        }
+
+        private static string Escape(string str)
+        {
+            string escapeStr = str.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("&#34;", "&quot;").Replace("'", "&apos;");
+            return escapeStr;
+        }
+    }
+}
