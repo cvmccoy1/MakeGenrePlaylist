57703be18441bbb684881303d45401add07a613a
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-01-11 13:12:36 -0700

    Replacing the XmlWriter with a StreamWriter.  The XmlWriter does it own XML escaping and it was interfering with my own.

--- XmlHelper.cs
+++ XmlHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security;
 using System.Text;
@@ -15,53 +16,43 @@ namespace MakeGenrePlaylist
     {
         public static void CreatePlaylist(string name, string filePath, ArrayList musicFiles)
         {
-            XmlWriterSettings settings = new XmlWriterSettings()
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
             {
-                Indent = true,
-                OmitXmlDeclaration = true,
-            };
-
-            using (XmlWriter writer = XmlWriter.Create(filePath, settings))
-            {
-                writer.WriteStartDocument();
-                writer.WriteRaw("<?wpl version=\"1.0\"?>\n");
-                writer.WriteStartElement("smil");
-                writer.WriteStartElement("head");
-                writer.WriteStartElement("meta");
-                writer.WriteAttributeString("name", "Generator");
-                writer.WriteAttributeString("content", "Microsoft Windows Media Player -- 12.0.19041.1566");
-                writer.WriteEndElement();
-                writer.WriteStartElement("meta");
-                writer.WriteAttributeString("name", "ItemCount");
-                writer.WriteAttributeString("content", musicFiles.Count.ToString());
-                writer.WriteEndElement();
-                writer.WriteElementString("title", name);
-                writer.WriteEndElement(); // head
-                writer.WriteStartElement("body");
-                writer.WriteStartElement("seq");
-                foreach (string file in musicFiles)
+                using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
                 {
-                    writer.WriteStartElement("media");
-                    writer.WriteAttributeString("src", Escape(file));
-                    writer.WriteAttributeString("albumTitle", Escape(FileHelper.GetExtendedFileProperty(file, "Album")));
-                    writer.WriteAttributeString("albumArtist", Escape(FileHelper.GetExtendedFileProperty(file, "Contributing artists")));
-                    writer.WriteAttributeString("trackTitle", Escape(FileHelper.GetExtendedFileProperty(file, "Title")));
-                    writer.WriteAttributeString("trackArtist", Escape(FileHelper.GetExtendedFileProperty(file, "Authors")));
-                    string lengthInTime = FileHelper.GetExtendedFileProperty(file, "Length");
-                    TimeSpan timeSpan = TimeSpan.ParseExact(lengthInTime, @"hh\:mm\:ss", CultureInfo.InvariantCulture);
-                    writer.WriteAttributeString("duration", timeSpan.TotalMilliseconds.ToString());
-                    writer.WriteEndElement(); // media
+                    writer.WriteLine("<?wpl version=\"1.0\"?>");
+                    writer.WriteLine("<smil>");
+                    writer.WriteLine("\t<head>");
+                    writer.WriteLine("\t\t<meta name = \"Generator\" content = \"Microsoft Windows Media Player -- 12.0.19041.1566\" />");
+                    writer.WriteLine($"\t\t<meta name=\"ItemCount\" content=\"{ musicFiles.Count}\" />");
+                    writer.WriteLine($"\t\t<title>{name}</title>");
+                    writer.WriteLine("\t</head>");
+                    writer.WriteLine("\t<body>");
+                    writer.WriteLine("\t\t<seq>");
+
+                    foreach (string file in musicFiles)
+                    {
+                        writer.Write($"\t\t\t<media src=\"{Escape(file)}\" ");
+                        writer.Write($"albumTitle=\"{Escape(FileHelper.GetExtendedFileProperty(file, "Album"))}\" ");
+                        writer.Write($"albumArtist=\"{Escape(FileHelper.GetExtendedFileProperty(file, "Contributing artists"))}\" ");
+                        writer.Write($"trackTitle=\"{Escape(FileHelper.GetExtendedFileProperty(file, "Title"))}\" ");
+                        writer.Write($"trackArtist=\"{Escape(FileHelper.GetExtendedFileProperty(file, "Authors"))}\" ");
+                        string lengthInTime = FileHelper.GetExtendedFileProperty(file, "Length");
+                        TimeSpan timeSpan = TimeSpan.ParseExact(lengthInTime, @"hh\:mm\:ss", CultureInfo.InvariantCulture);
+                        writer.Write($"duration=\"{timeSpan.TotalMilliseconds}\" ");
+                        writer.WriteLine("/>");
+                    }
+
+                    writer.WriteLine("\t\t</seq>");
+                    writer.WriteLine("\t</body>");
+                    writer.WriteLine("</smil>");
                 }
-                writer.WriteEndElement(); // body
-                writer.WriteEndElement(); // seq
-                writer.WriteEndElement(); // smil
-                writer.WriteEndDocument();
             }
         }
 
         private static string Escape(string str)
         {
-            string escapeStr = str.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("&#34;", "&quot;").Replace("'", "&apos;");
+            string escapeStr = str.Replace("\"", "'").Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("&#34;", "&quot;").Replace("'", "&apos;");
             return escapeStr;
         }
     }
