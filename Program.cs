bbab6e3f58f679f09ccf984798554ad0e466faa4
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-01-11 20:50:28 -0700

    Adding command line support.

--- Program.cs
+++ Program.cs
@@ -5,24 +5,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace MakeGenrePlaylist
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            if (args is null)
+            RootCommand rootCommand = new RootCommand
             {
-                throw new ArgumentNullException(nameof(args));
-            }
-            const string path = @"\\MCCOY-NAS\Public\All Family Stuff\Vana's Music\";
+                new Option<string>(
+                    new [] { "--fullpath", "-f" },
+                    "Specify the path to search."),
+                new Option<string>(
+                    new [] { "--property", "-p" },
+                    "Specify the property to filter on."),
+                new Option<string>(
+                    new [] { "--value", "-v" },
+                    "Specify the property's value.")
+            };
+            rootCommand.Description = "App to search for files with a specific property and creates a Windows playlist file (.wpl)";
+
+            rootCommand.Handler = CommandHandler.Create<string, string, string>(Execute);
+            // Parse the incoming args and invoke the handler
+            return rootCommand.Invoke(args);
+        }
+
+        private static void Execute(string fullpath, string property, string value)
+        {
+            string path = String.IsNullOrWhiteSpace(fullpath) ? @"\\MCCOY-NAS\Public\All Family Stuff\Vana's Music\" : fullpath;
+            string fileProperty = String.IsNullOrWhiteSpace(property) ? "Genre" : property;
+            string filePropertyValue = String.IsNullOrWhiteSpace(value) ? "Romance" : value;
+
             List<string> searchExt = new List<string>()
             {
                 ".m4a", ".mp3",  ".mp4", ".wav", ".wma", ".aac"
             };
-            const string fileProperty = "Genre";
-            const string filePropertyValue = "Romance";
 
             Console.Write("Finding All Music Files...");
             ArrayList musicFiles = FileHelper.GetFilesBySearchPattern(path, searchExt);
@@ -32,8 +52,9 @@ namespace MakeGenrePlaylist
             List<Dictionary<string, string>> genreFiles = FileHelper.GetFilePropertiesBySpecificProperty(musicFiles, fileProperty, filePropertyValue);
             Console.WriteLine($"Found {genreFiles.Count} Files!");
 
-            Console.Write("Creating Play List File...");
-            XmlHelper.CreatePlaylist("Romance", Path.Combine(path, "Romance.wpl"), genreFiles);
+            string playlistName = filePropertyValue.Trim().Replace(" ", "_");
+            Console.Write($"Creating Play List File {playlistName}...");
+            XmlHelper.CreatePlaylist($"{playlistName}", Path.Combine(path, $"{playlistName}.wpl"), genreFiles);
             Console.WriteLine("Done!");
         }
     }
